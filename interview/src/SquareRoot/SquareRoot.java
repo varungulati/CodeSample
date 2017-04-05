87b353ac86229179a76de753558c7c82008da8d7
Varun Gulati <Varun Gulati@DESKTOP-QOLASS6.fios-router.home>
2017-04-05 18:13:42 -0400

    FB

--- interview/src/SquareRoot/SquareRoot.java
+++ interview/src/SquareRoot/SquareRoot.java
@@ -15,8 +15,39 @@ public class SquareRoot {
 		}
 		return x;
 	}
+	/**
+	 * @param num
+	 * @param lo
+	 * @param hi
+	 * @param precision
+	 * @return
+	 */
+	private static float mySqrtHelper(float num, float lo, float hi, float precision) {
+		if(lo > hi) {
+			return lo;
+		}
+		float mid = lo + (hi - lo)/2;
+		
+		if(mid > num/mid) {
+			return mySqrtHelper(num, lo, mid - precision, precision);
+		} 
+		else if(mid + precision > num/(mid+ precision)) {
+			return mid + precision;
+		}
+		else {
+			return mySqrtHelper(num, mid + precision, hi, precision);
+		}
+	}
+    /**
+     * @param num
+     * @return
+     */
+    public static float mySqrt(float num) {
+    	return mySqrtHelper(num, 0, Float.MAX_VALUE, 0.0001F);
+    }
 	public static void main(String[] args) {
-		System.out.println(SquareRoot.squareRoot(30));
+		System.out.println(SquareRoot.squareRoot(25));
+		System.out.println(SquareRoot.mySqrt(25));
 	}
 
 }
